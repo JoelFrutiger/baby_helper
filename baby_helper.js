2d4f1900502f8f81c0edbb1fe27b9d435c3e5980
eljoe <jooolchef@gmail.com>
2022-02-10 17:43:37 +0100

    Create API for the baby-tracker calls. Refactor some of the code.

--- baby_helper.js
+++ baby_helper.js
@@ -5,152 +5,182 @@ var Promise = require("bluebird");
 var LOGIN = 'https://prodapp.babytrackers.com/session';
 var DEVICES = 'https://prodapp.babytrackers.com/account/device';
 var TRANSACTION = 'https://prodapp.babytrackers.com/account/transaction';
-var USERNAME = 'USERNAME';
-var PASSWORD ='PASSWORD';
-var UUID = 'fe087824-dc84-44f3-809a-c01f945e4057';
+var USERNAME;
+var PASSWORD;
+var UUID;
 const uuidV4 = require('uuid/v4');
 var dateFormat = require('dateformat');
 var fs = require('fs');
-var syncIdFile = "/var/tmp/babytracker-syncid"
+var syncIdFile;
 
-function BabyHelper() {
-    
-
+function BabyHelper(username, password, uuid, syncFile) {
+    if(!username || !password ){
+        console.error("Pass username and password")
+    }
+    USERNAME = username;
+    PASSWORD = password;
+    UUID = uuid;
+    syncIdFile = syncFile;
 }
 
-BabyHelper.prototype.login = function() {
+BabyHelper.prototype.login = function () {
     var data = {
-    AppInfo: { AccountType: 0, AppType: 0 },
-    Device: { DeviceName: "Unknown", DeviceOSInfo: "Nexus 5X OS25", DeviceUUID: UUID },
-    EmailAddress: USERNAME,
-    Password: PASSWORD
+        AppInfo: { AccountType: 0, AppType: 0 },
+        Device: { DeviceName: "Unknown2", DeviceOSInfo: "Nexus 5X OS225", DeviceUUID: UUID },
+        EmailAddress: USERNAME,
+        Password: PASSWORD
+    };
+    var self = this;
+    return this.postRequest(LOGIN, data).then(function (resp1) {
+        return self.getRequest(DEVICES);
+    }).then(function (resp2) {
+        var deviceList = resp2.body;
+        if (deviceList.length > 0) {
+            //console.log(deviceList);
+            return true;
+        } else {
+            console.log(deviceList);
+            throw new Error("Unable to Retrieve Devices With Session");
+            return false;
+        }
+    }).catch(function (err) {
+        console.log(err);
+
+        throw new Error("Unable to Retrieve Session");
+    });
 };
-    var self=this;
-    return this.postRequest(LOGIN,data).then(function (resp1) {
-            return self.getRequest(DEVICES);
-        }).then(function (resp2) {
-            var deviceList=resp2.body;
-                if(deviceList.length > 0)
-                {
-                //console.log(deviceList);
-                return true;
-                } else {
-                    //console.log(resp2);
-                    throw new Error("Unable to Retrieve Devices With Session");
-                    return false;
-                }
-                 }).catch(function (err) {
-                  //console.log(err)
-                //console.log(err);
-
-                 throw new Error("Unable to Retrieve Session");
-                });
-            };
 
 
-BabyHelper.prototype.getBabyObject = function( ) {
-    var self=this;
-    return this.getRequest(DEVICES).then(function (response){
-        //console.log("Grabbed Devices");
+BabyHelper.prototype.getBabyObject = function (log) {
+    var self = this;
+    return this.getRequest(DEVICES).then(function (response) {
         var deviceList = response.body;
         var latestTransactions = [];
-        for(var i=0;i<deviceList.length;i++)
-        {
-            latestTransactions.push(TRANSACTION+"/"+deviceList[i].DeviceUUID+"/"+(deviceList[i].LastSyncID-1));
+        for (var i = 0; i < deviceList.length; i++) {
+            latestTransactions.push(TRANSACTION + "/" + deviceList[i].DeviceUUID + "/" + (deviceList[i].LastSyncID - 1));
         }
-        //console.log(latestTransactions);
-        return Promise.map(latestTransactions, function(obj) {
-              return self.getRequest(obj).then(function(response) {
+        if(log){
+            console.log("latest transactions", latestTransactions);
+        }
+        return Promise.map(latestTransactions, function (obj) {
+            return self.getRequest(obj).then(function (response) {
                 var rawTransaction = JSON.parse(Buffer.from(response.body[0].Transaction, 'base64').toString());
+                if(log){
+                    console.log("RawTransaction: ", rawTransaction)
+                }
                 return (rawTransaction);
-                });
+            });
         });
-    }).then(function(results) {
-              //console.log(results);
-              var babyResult = "";
-              var latestDate = Date.parse("January 1, 1970");
-              for (var i = 0; i < results.length; i++) {
-               
-                 if(Date.parse(results[i].timestamp) > latestDate)
-                 {
-                    latestDate = Date.parse(results[i].timestamp);
-                    babyResult = results[i].baby;
-                 }
-              }
+    }).then(function (results) {
+        //console.log("Results", results);
+        var babyResult = "";
+        var latestDate = Date.parse("January 1, 1970");
+        for (var i = 0; i < results.length; i++) {
 
-              return babyResult;
-          }).catch(function (err) {
+            if (Date.parse(results[i].timestamp) > latestDate) {
+                latestDate = Date.parse(results[i].timestamp);
+                babyResult = results[i].baby;
+            }
+        }
+        //console.log("BabyResult", babyResult);
+
+        return babyResult;
+    }).catch(function (err) {
         console.log(err);
         throw new Error("Unable To Retrieve Baby Object");
     })
-    
+
 };
 
-BabyHelper.prototype.logDiaper = function(changeType) {
-    var self=this;
-    return this.login().then(function(loginState)
-    {
-        return self.getBabyObject();
-    }).then(function(babyObj){
+BabyHelper.prototype.testStuff = function () {
+    var self = this;
+    this.login().then(function (loginState) {
+        return self.getBabyObject(true);
+    });
+}
+
+BabyHelper.prototype.logDiaper = function (changeType) {
+    var self = this;
+    return this.login().then(function (loginState) {
+        return self.getBabyObject(false);
+    }).then(function (babyObj) {
 
         var myUUID = uuidV4();
         var now = new Date();
-        var timestamp = dateFormat(now, "yyyy-mm-dd HH:MM:ss +0000");
+        var utc = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
+        var timestamp = dateFormat(utc, "yyyy-mm-dd HH:MM:ss +0000");
         var data = {
-                      BCObjectType: "Diaper",
-                      amount: "2",
-                      status: changeType,
-                      baby: babyObj,
-                      note: "",
-                      pictureLoaded: true,
-                      pictureNote: [],
-                      time: timestamp,
-                      newFlage: true,
-                      objectID: myUUID,
-                      timestamp: timestamp
-            };
-        var dataBuffer = new Buffer(JSON.stringify(data)).toString("base64");
-        var currentSyncId = fs.readFileSync(syncIdFile).toString();
-        var newSyncId = parseInt(currentSyncId)+1;
+            BCObjectType: "Diaper",
+            pooColor: 5,
+            peeColor: 5,
+            texture: 5,
+            amount: 2,
+            flag: 0,
+            status: changeType,
+            baby: babyObj,
+            note: "",
+            pictureLoaded: true,
+            pictureNote: [],
+            time: timestamp,
+            newFlage: true,
+            objectID: myUUID,
+            timestamp: timestamp
+        };
+        var json = JSON.stringify(data);
+        var dataBuffer = Buffer.from(json).toString("base64");
+        var currentSyncId = 0
+        try {
+            currentSyncId = fs.readFileSync(syncIdFile).toString() ?? 0;
+        } catch(e){
+            console.log(e)
+        }
+        var newSyncId = parseInt(currentSyncId) + 1;
         var finalTransaction = {
             OPCode: 0,
             SyncID: newSyncId,
             Transaction: dataBuffer
         };
-        return self.postRequest(TRANSACTION,finalTransaction);
-    }).then(function(response){
+        console.log("Final transaction", finalTransaction)
+        return self.postRequest(TRANSACTION, finalTransaction);
+    }).then(function (response) {
         console.log("Logged Diaper Change - " + changeType);
-        var currentSyncId = fs.readFileSync(syncIdFile).toString();
-        var newSyncId = parseInt(currentSyncId)+1;
-        fs.writeFileSync(syncIdFile,newSyncId);
+        var currentSyncId = 0;
+        try {
+            currentSyncId = fs.readFileSync(syncIdFile).toString() ?? 0;
+        }
+        catch(e){
+            console.log(e);
+        }
+        var newSyncId = parseInt(currentSyncId) + 1;
+        fs.writeFileSync(syncIdFile, newSyncId.toString(),{ flag:'w+' });
         return true;
-        }).catch(function(err){
-    throw new Error("Unable to Log Diaper Change" + err)
-});
+    }).catch(function (err) {
+        throw new Error("Unable to Log Diaper Change" + err)
+    });
 }
 
-BabyHelper.prototype.getRequest = function(url) {
-  var options = {
-    method: 'GET',
-    uri: url,
-    resolveWithFullResponse: true,
-    json: true,
-    jar: true
-  };
-  
-  return rp(options);
-};  
+BabyHelper.prototype.getRequest = function (url) {
+    var options = {
+        method: 'GET',
+        uri: url,
+        resolveWithFullResponse: true,
+        json: true,
+        jar: true
+    };
 
-BabyHelper.prototype.postRequest = function(url,data) {
-  var options = {
-    url: url,
-    method: "POST",
-    resolveWithFullResponse: true,
-    json: data,
-    jar: true
-  };
+    return rp(options);
+};
 
-  return rp(options);
-}; 
+BabyHelper.prototype.postRequest = function (url, data) {
+    var options = {
+        url: url,
+        method: "POST",
+        resolveWithFullResponse: true,
+        json: data,
+        jar: true
+    };
+
+    return rp(options);
+};
 
 module.exports = BabyHelper;
--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-var DashButton = require('dash-button');
 var BabyHelper = require('./baby_helper.js');
-var babyHelper = new BabyHelper();
+// you might want to change this.
+var UUID = 'f7cdc154-c1b6-4c49-b8f6-051783cf8a85';
+var syncIdFile = "/tmp/babytracker-syncid"
+var babyHelper = new BabyHelper(process.argv[2], process.argv[3], UUID, syncIdFile);
 var dateFormat = require('dateformat');
 var lastEvent = new Date("January 01, 2000 00:00:00");
-const util = require('util')
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
 
-//Pee (Elements) -  40:b4:cd:28:82:a2
-//Poop (Battery) - 68:54:fd:80:77:78
-//Mixed (Battery) - 68:54:fd:3e:31:1f
+app.get("/poo", (req, res, next) => {
+    if(checkBtnPress()){
+        babyHelper.logDiaper('1');
+        res.sendStatus(200);
+    }
+    else{
+        res.sendStatus(500);
+    }
+});
 
-var peeBtn = new DashButton("40:b4:cd:28:82:a2");
-var pooBtn = new DashButton("68:54:fd:80:77:78");
-var mixedBtn = new DashButton("68:54:fd:3e:31:1f");
-
-console.log("Running!");
-
-var registerPee = peeBtn.addListener(function(){
-    var timestamp = checkBtnPress();
-    if(timestamp)
-    {
-        console.log("Pee Occured At: " + timestamp);
+app.get("/pee", (req, res, next) => {
+    if(checkBtnPress()){
         babyHelper.logDiaper(0);
+        res.sendStatus(200);
+    }
+    else{
+        res.sendStatus(500);
     }
 });
 
-var registerPoo = pooBtn.addListener(function(){
-    var timestamp = checkBtnPress();
-    if(timestamp)
-    {
-        console.log("Poo Occured At: " + timestamp);
-        babyHelper.logDiaper(1);
+app.get("/mixed", (req, res, next) => {
+    if(checkBtnPress()){
+        babyHelper.logDiaper('2');
+        res.sendStatus(200);
+    }
+    else{
+        res.sendStatus(500);
     }
 });
 
-var registerMixed = mixedBtn.addListener(function(){
-    var timestamp = checkBtnPress();
-    if(timestamp)
-    {
-        console.log("Mixed Occured At: " + timestamp);
-        babyHelper.logDiaper(2);
-    }
+app.get("/test", (req, res, next) => {
+    babyHelper.testStuff();
+    res.sendStatus(200);
 });
 
 function checkBtnPress() {
     var now = new Date();
-    var checkTime = new Date(lastEvent.setTime(lastEvent.getTime()+ 1000 * 60));
-    if(checkTime > now) { 
-            //console.log("Duplicate Button Press");
-            return false;
-        } else {
-            var timestamp = dateFormat(now, "yyyy-mm-dd HH:MM:ss");
-            lastEvent.setTime(now.getTime());
-            return timestamp;
-        }
+    var checkTime = new Date(lastEvent.getTime() + 1000 * 60);
+    if (checkTime > now) {
+        console.log("Duplicate Button Press", checkTime);
+        return false;
+    } else {
+        var timestamp = dateFormat(now, "yyyy-mm-dd HH:MM:ss");
+        lastEvent.setTime(now.getTime());
+        return timestamp;
+    }
 }
